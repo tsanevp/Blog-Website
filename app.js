774abe5bc76b344f8e5e28121abbc5c780a4f505
tsanevp <petertsanev7@gmail.com>
2023-07-03 21:22:54 -0700

    refactored folder structure

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import bodyParser from 'body-parser';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
+const homeStartingContent = "Lorem ipsum dolor sit amet. Ea quae mollitia aut quod excepturi qui maxime rerum ad minus temporibus utaliquam iusto ut corrupti galisum. Rem repellendus internos qui asperiores ducimus ea repellendus maximeet quasi inventore et inventore rerum ut explicabo reiciendis qui corporis aspernatur. Et omnisperferendis non quaerat sapiente est eveniet natus aut maxime molestias ut sunt magni non animivoluptatem aut tempora nihil.Hic incidunt molestias in voluptas sequi At necessitatibus galisum non enimtenetur eos rerum alias At quos libero. Est illo perferendis sit voluptatem quia cum provident debitiseos iusto necessitatibus.Intempore labore et sequi accusantium ea facilis alias. Qui voluptates nihil ut expedita velit sedautem laudantium et saepe optio. Et voluptas nisi sit deserunt labore et corporis omnis qui odit maximehic neque deleniti nam ullam quisquam non praesentium atque. Sed tempora autem est quia voluptatem nonminus eveniet.";
+const aboutContent = "Lorem ipsum dolor sit amet. Ea quae mollitia aut quod excepturi qui maxime rerum ad minus temporibus utaliquam iusto ut corrupti galisum. Rem repellendus internos qui asperiores ducimus ea repellendus maximeet quasi inventore et inventore rerum ut explicabo reiciendis qui corporis aspernatur. Et omnisperferendis non quaerat sapiente est eveniet natus aut maxime molestias ut sunt magni non animivoluptatem aut tempora nihil.Hic incidunt molestias in voluptas sequi At necessitatibus galisum non enimtenetur eos rerum alias At quos libero. Est illo perferendis sit voluptatem quia cum provident debitiseos iusto necessitatibus.Intempore labore et sequi accusantium ea facilis alias. Qui voluptates nihil ut expedita velit sedautem laudantium et saepe optio. Et voluptas nisi sit deserunt labore et corporis omnis qui odit maximehic neque deleniti nam ullam quisquam non praesentium atque. Sed tempora autem est quia voluptatem nonminus eveniet.";
+const contactContent = "Lorem ipsum dolor sit amet. Ea quae mollitia aut quod excepturi qui maxime rerum ad minus temporibus utaliquam iusto ut corrupti galisum. Rem repellendus internos qui asperiores ducimus ea repellendus maximeet quasi inventore et inventore rerum ut explicabo reiciendis qui corporis aspernatur. Et omnisperferendis non quaerat sapiente est eveniet natus aut maxime molestias ut sunt magni non animivoluptatem aut tempora nihil.Hic incidunt molestias in voluptas sequi At necessitatibus galisum non enimtenetur eos rerum alias At quos libero. Est illo perferendis sit voluptatem quia cum provident debitiseos iusto necessitatibus.Intempore labore et sequi accusantium ea facilis alias. Qui voluptates nihil ut expedita velit sedautem laudantium et saepe optio. Et voluptas nisi sit deserunt labore et corporis omnis qui odit maximehic neque deleniti nam ullam quisquam non praesentium atque. Sed tempora autem est quia voluptatem nonminus eveniet.";
+
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -14,30 +18,31 @@ app.use(express.static("public"));
 
 app.set('view engine', "ejs");
 
-app.get('/', function(req, res) {
-    res.render('home', { blogPosts: blogPosts });
+app.get('/', function (req, res) {
+    res.render('home', { blogPosts: blogPosts, homeStartingContent: homeStartingContent });
 });
 
-app.get('/about', function(req, res) {  
-    res.render('about');
+app.get('/about', function (req, res) {
+    res.render('about', { aboutContent: aboutContent });
 });
 
-app.get('/contact', function(req, res) {  
-    res.render('contact');
+app.get('/contact', function (req, res) {
+    res.render('contact', { contactContent: contactContent});
 });
 
-app.get('/compose', function(req, res) {  
+app.get('/compose', function (req, res) {
     res.render('compose');
 });
 
-app.post('/compose', function(req, res) {
+app.post('/compose', function (req, res) {
     const body = req.body;
     const title = body.title;
     const content = body.content;
     blogPosts.push([title, content])
+    
     res.redirect('/')
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Server is running on port 3000");
 })
